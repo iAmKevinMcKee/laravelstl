746766c97d185eabaa935b077c745f32514d8979
Kevin <kevin@intellow.com>
2019-09-04 22:53:03 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/future-topics', [\App\Http\Controllers\FutureTopicsController::clas
 
 Route::get('/dashboard', 'DashboardController@index')->middleware('auth');
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function () {
+    return view('nothere');
+});
