f3a916622e2982b206b21b827ff0393d7f6b6a65
John <sbcman74@gmail.com>
2019-09-17 04:49:29 -0500

    updated focus treatment on add new topic button and filters to have opacity-75 similiar to hover state, converted score arrow icons to buttons for easier keyboard navigation

--- public/js/app.js
+++ public/js/app.js
@@ -15681,8 +15681,8 @@ window.axios.defaults.headers.common = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/kevinmckee/Code/laravelstl/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/kevinmckee/Code/laravelstl/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Users/JohnLowery/Code/laravelstl/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/JohnLowery/Code/laravelstl/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
