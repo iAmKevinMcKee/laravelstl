61d182d37bc7da2588357435983dd0a85f3b0349
John <sbcman74@gmail.com>
2019-09-26 20:50:36 -0500

    updated red checkmark to stl red color

--- resources/views/livewire/event.blade.php
+++ resources/views/livewire/event.blade.php
@@ -8,7 +8,7 @@
 
     </button>
     <div class="text-4xl" wire:model="score">{{$score}}</div>
-    <button wire:click="vote(-1)" class="{{$userVote === -1 ? 'text-red-700 hover:text-red-600 focus:outline-none' :'text-blue-800 hover:text-blue-600  focus:outline-none'}}">
+    <button wire:click="vote(-1)" class="{{$userVote === -1 ? 'text-stl-red hover:text-red-600 focus:outline-none' :'text-blue-800 hover:text-blue-600  focus:outline-none'}}">
       @if($userVote === -1) 
             <i class="fas fa-check-circle cursor-pointer"></i>
              @else 
