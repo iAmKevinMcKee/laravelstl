2ccb33af1e9878f51ccb7aa05d17ab131fff06d8
John <sbcman74@gmail.com>
2019-10-28 19:43:45 -0500

    Added mask and transition for user edit modal. Minor updates to the save button on the modal.

--- resources/views/livewire/_editUserModal.blade.php
+++ resources/views/livewire/_editUserModal.blade.php
@@ -1,10 +1,11 @@
-  <div class="bg-gray-300 fixed inset-0 flex items-center justify-center">
-        <button class="absolute top-0 right-0 mt-20 mr-20 text-gray-700 text-xl flex items-center"
+<div class="relative" wire:transition.fade.300ms>
+    <div class="modal-mask fixed inset-0 flex items-center justify-center">
+        <button class="absolute top-0 right-0 mt-20 mr-20 text-gray-300 text-xl flex items-center"
             wire:click="stopEditing">
             <i class="fas fa-times"></i>
             <span class="ml-2">Close</span>
         </button>
-        <div class="w-1/2 bg-white shadow-lg p-8 rounded">
+        <div class="w-1/2 bg-white shadow-lg p-8 rounded z-10">
             <h3 class="text-xl mb-8">Manage User</h3>
             <div class="mb-4 flex items-center ">
                 <label for="name" class="mr-2 w-32 text-right font-bold">Name:</label>
@@ -28,10 +29,11 @@
             <div class="flex items-center justify-end">
                 <button class="underline text-gray-700 mr-8" wire:click="stopEditing">Cancel</button>
                 <button
-                    class="bg-blue-800 p-2 rounded focus:outline-none uppercase font-bold hover:bg-blue-600 text-white"
+                    class="bg-blue-800 px-4 py-2 rounded focus:outline-none hover:bg-blue-600 text-white"
                     wire:click="saveUser">
-                    Save User
+                    Save
                 </button>
             </div>
         </div>
     </div>
+</div>
