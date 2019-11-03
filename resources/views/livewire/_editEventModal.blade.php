f441f3802c1b2e11007f482c20d28886dbb414b2
Lowery <jlowery1@kstlmacrdwng8wp.utopia.net>
2019-11-03 13:49:34 -0600

    Finished edit event feature

--- resources/views/livewire/_editEventModal.blade.php
+++ resources/views/livewire/_editEventModal.blade.php
@@ -1,12 +1,12 @@
 <div class="relative" wire:transition.fade.300ms>
-    <div class="modal-mask fixed inset-0 flex items-center justify-center z-10">
-        <div class="bg-gray-300 w-1/2 p-8 fixed flex items-center justify-center">
+    <div class="modal-mask fixed inset-0 flex items-center justify-center">
+        <div class="fixed w-full h-full flex items-center justify-center">
             <button class="absolute top-0 right-0 mt-20 mr-20 text-gray-300 text-xl flex items-center"
                 wire:click="stopEditing">
                 <i class="fas fa-times"></i>
                 <span class="ml-2">Close</span>
             </button>
-            <div class="w-1/2 bg-white shadow-lg p-8 rounded z-10">
+            <div class="bg-white shadow-lg rounded p-8 w-1/2">
                 <h3 class="text-xl mb-8">Manage Event</h3>
                 <div class="mb-4 flex items-center ">
                     <label for="name" class="mr-2 w-32 text-right font-bold">Name:</label>
@@ -15,7 +15,7 @@
                 </div>
                 <div class="flex items-center mb-4 ">
                     <label for="email" class="mr-2 w-32 text-right font-bold">Date:</label>
-                    <input type="date" class="p-2 border rounded w-full" wire:model="editedEvent.date"
+                    <input type="date" class="p-2 border rounded w-full truncate" wire:model="editedEvent.date"
                         value="{{$eventBeingEdited->date->format('Y-m-d')}}">
                 </div>
                 <div class="flex items-center mb-4">
